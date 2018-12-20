7d669826a1656ddd0b7206122fa2fb3b35d45e25
Volrest <olena.melnikova@gmail.com>
2018-12-20 17:41:32 +0300

    1

--- Easy Chat/Form1.Designer.cs
+++ Easy Chat/Form1.Designer.cs
@@ -28,13 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.logoutButton = new System.Windows.Forms.Button();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.chatTextBox = new System.Windows.Forms.TextBox();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 24F);
+            this.label1.Location = new System.Drawing.Point(29, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Имя:";
+            // 
+            // loginButton
+            // 
+            this.loginButton.Font = new System.Drawing.Font("Consolas", 24F);
+            this.loginButton.Location = new System.Drawing.Point(564, 41);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(253, 58);
+            this.loginButton.TabIndex = 1;
+            this.loginButton.Text = "Войти";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.Font = new System.Drawing.Font("Consolas", 24F);
+            this.logoutButton.Location = new System.Drawing.Point(823, 41);
+            this.logoutButton.Name = "logoutButton";
+            this.logoutButton.Size = new System.Drawing.Size(253, 58);
+            this.logoutButton.TabIndex = 2;
+            this.logoutButton.Text = "Выйти";
+            this.logoutButton.UseVisualStyleBackColor = true;
+            this.logoutButton.Click += new System.EventHandler(this.logoutButton_Click);
+            // 
+            // sendButton
+            // 
+            this.sendButton.Font = new System.Drawing.Font("Consolas", 24F);
+            this.sendButton.Location = new System.Drawing.Point(36, 823);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(1040, 58);
+            this.sendButton.TabIndex = 3;
+            this.sendButton.Text = "Отправить сообщение";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // chatTextBox
+            // 
+            this.chatTextBox.Font = new System.Drawing.Font("Consolas", 24F);
+            this.chatTextBox.Location = new System.Drawing.Point(36, 109);
+            this.chatTextBox.Multiline = true;
+            this.chatTextBox.Name = "chatTextBox";
+            this.chatTextBox.Size = new System.Drawing.Size(1040, 657);
+            this.chatTextBox.TabIndex = 4;
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Font = new System.Drawing.Font("Consolas", 24F);
+            this.messageTextBox.Location = new System.Drawing.Point(36, 772);
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(1040, 45);
+            this.messageTextBox.TabIndex = 5;
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Font = new System.Drawing.Font("Consolas", 24F);
+            this.userNameTextBox.Location = new System.Drawing.Point(124, 49);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(434, 45);
+            this.userNameTextBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1095, 893);
+            this.Controls.Add(this.userNameTextBox);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.chatTextBox);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.logoutButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Easy Chat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button logoutButton;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox chatTextBox;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.TextBox userNameTextBox;
     }
 }
 
